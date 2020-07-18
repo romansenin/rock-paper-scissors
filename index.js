38a25e6bcba6c44c870fd13c01db74302efdb6b1
Roman Senin <romansenin15@gmail.com>
2020-07-18 13:35:26 -0700

    added restart button

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const selectionDiv = document.querySelector(".selection");
 const battleDiv = document.querySelector(".battle");
-const userImg = battleDiv.querySelector("img");
-const computerImg = battleDiv.querySelector(".box:last-child img");
+const userBox = battleDiv.querySelector(".box");
+const userImg = userBox.querySelector("img");
+const computerBox = battleDiv.querySelector(".box:last-child");
+const computerImg = computerBox.querySelector("img");
+const restartBtn = document.getElementById("btn-restart");
+
+let numWins = 0;
+let numLosses = 0;
+let numTies = 0;
 
 document.querySelectorAll(".selection .box").forEach((box) => {
   box.onclick = handleClick;
 });
+
+restartBtn.onclick = function() {
+  this.style.display = "none";
+  battleDiv.style.display = "none";
+  selectionDiv.style.display = "flex";
+  userBox.style.backgroundColor = "#fff";
+  computerBox.style.backgroundColor = "#fff";
+}
 
 function handleClick() {
   selectionDiv.style.display = "none";
@@ -30,30 +45,50 @@ function choose(lastChoice) {
 }
 
 async function playGame(userChoice) {
-  const computerChoice = (await decisionWheel(15)).charAt(0);
+  // spin off the decision wheel 20 times, get first character of resulting string
+  const computerChoice = (await decisionWheel(20)).charAt(0);
+
   if (userChoice === computerChoice) {
+    // tie
+    numTies++;
+    // set both user's and computer's boxes to green background color
     battleDiv.querySelector(".box").style.backgroundColor = "#00ff00";
-    battleDiv.querySelector(".box:last-child").style.backgroundColor = "#00ff00";
+    battleDiv.querySelector(".box:last-child").style.backgroundColor =
+      "#00ff00";
   } else if (
+    // user wins
     (userChoice === "R" && computerChoice === "S") ||
     (userChoice === "P" && computerChoice === "R") ||
     (userChoice === "S" && computerChoice === "P")
   ) {
+    numWins++;
+    // set user's box to green background color
     battleDiv.querySelector(".box").style.backgroundColor = "#00ff00";
   } else {
-    battleDiv.querySelector(".box:last-child").style.backgroundColor = "#00ff00";
+    // computer wins
+    numLosses++;
+    // set computer's box to green background color
+    battleDiv.querySelector(".box:last-child").style.backgroundColor =
+      "#00ff00";
   }
+  restartBtn.style.display = "block";
 }
 
 function decisionWheel(numTimes, count = 1, lastChoice = "Rock") {
   return new Promise((resolve) => {
+    // set off a function call after 180 * 0.1 * count milliseconds, the higher the count, the slower it gets
     setTimeout(function () {
+      // choose either R, P, or S, excluding the last choice so that a change is visible in the computer's box
       const choice = choose(lastChoice);
+
+      // set the computer box's image and alt tag
       computerImg.src = generateSrc(choice);
       computerImg.alt = choice;
+      // if not done yet, recursively resolve itself
       if (count < numTimes)
         return resolve(decisionWheel(numTimes, ++count, choice));
+      // else resolve the final choice
       resolve(choice);
-    }, 200 * 0.1 * count);
+    }, 180 * 0.1 * count);
   });
 }
